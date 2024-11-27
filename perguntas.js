f874d883207efb056a541087b60a44a414f5d082
DiogoStoso <165948768+DiogoEnzoS@users.noreply.github.com>
2024-11-27 16:20:23 -0300

    up

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Classicas do Brasil',
+    'Qual musica da Legião Urbana se tornou um verdadeiro hino da juventude brasileira na década de 1980?',
+    'A música "TEMPO  PERDIDO", lançada em 1986 '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Atuais do Brasil',
+    'Quem é o artista brasiliero responsavél pelo hit top spotify "Envolver"',
+    'Anitta. A música fez sucesso não só no Brasil, mas também internacionalmente'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Classicas do Brasil',
+    'Quem é conhecido como o "Rei da Música Popular Brasileira',
+    'Roberto Carlos.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Atuais do Brasil',
+    'Quem é conhecida como a Rainha da sofrencia?',
+    'Marilía Mendonça.'
 )
